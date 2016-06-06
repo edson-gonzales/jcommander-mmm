73fe4f23961c13c8412805c81dc99f0c850a0fa1
milevs <anelimv@gmail.com>
2016-06-06 08:31:47 -0400

    Add files via upload

---
+++ Rename.java
@@ -0,0 +1,37 @@
+
+import java.io.File;
+
+/**
+ * class Rename that has a boolean method renameFile 
+ * @author Milena Vallejos 
+ * @date May 24
+ */
+public class Rename
+{
+    /**
+     * Method to renameFile 
+     * @param actualName is a String (the origin name)
+     * @param toRename is a String (the new name)
+     * @return a boolean returns true if they could rename the file
+     * false if there was an error to rename
+     */
+	public static boolean RenameFile(String actualName,String toRename)
+	{
+		try
+        {
+            File initialName = new File(actualName);
+            File finalName = new File(toRename);
+
+            if (initialName.renameTo(finalName))
+            {
+                return true;
+            }
+        }
+             catch (IOException ioe)
+            {
+                return false;
+            }
+            
+	}
+	
+}
